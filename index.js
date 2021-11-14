48725c929466f3d85614c259bce58c359bb520ac
Artyom_Talatynnik <artsyomtalatynnik@gmail.com>
2021-11-14 17:26:54 +0300

    modified assert

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const assert = require('assert')
 describe('search hk50 test', function() {
     this.timeout(300000)
     let driver
-    //let WebDriverWait;
     let vars
     let login = "shkolar.neymeka@bk.ru"
     let password = "V$#C8BZC6Nn*5jr"
-    let expectedValue = "hk50"
+    let expectedValue = "HK50"
     let realvalue
-        //let wait = new WebDriverWait(driver, 10);
+
     beforeEach(async function() {
         driver = await new Builder().forBrowser('firefox').build()
         vars = {}
@@ -30,9 +29,12 @@ describe('search hk50 test', function() {
         await driver.findElement(By.name("search")).click()
         await driver.findElement(By.name("search")).sendKeys(expectedValue)
         await driver.sleep(3000);
-        realvalue = await driver.findElement(By.css(".item:nth-child(2) > .link")).innerHTML
+
         await driver.findElement(By.css(".item:nth-child(2) > .link")).click()
+        await driver.sleep(3000);
+
+        realvalue = await driver.findElement(By.css(".page_header_part > .title > h2")).getText()
         assert.equal(expectedValue, realvalue);
-        //await driver.close()
+        await driver.close()
     })
 })
