228a55289f2946cf5c4b06043e97acbf9f8f6132
Artyom_Talatynnik <artsyomtalatynnik@gmail.com>
2021-11-14 22:45:09 +0300

    delete pageobject because ne poluchilos

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
-    const Basepage = require('./pageobjects/basepage')
-const assert = require("assert");
+const assert = require('assert')
 
 
-describe('index.js', function() {
-
+describe('index js', function() {
+    this.timeout(300000)
     let driver
     let vars
     let login = "shkolar.neymeka@bk.ru"
     let password = "V$#C8BZC6Nn*5jr"
     let expectedValue = "HK50"
     let realvalue
-    var baseurl = "https://my.liteforex.com/ru?openPopup=%2Fru%2Flogin%2Fpopup%3FreturnUrl%3D%252Fru%252F"
 
     beforeEach(async function() {
-        this.timeout(300000)
+        driver = await new Builder().forBrowser('firefox').build()
+        vars = {}
     })
     afterEach(async function() {
         await driver.quit();
-        await driver.close()
     })
-    it('search hk50 test', async function(done) {
-        Basepage:this.timeout(300000)
-        setTimeout(done, 300000);
-        await Basepage.goToUrl(baseurl)
-        await Basepage.login(login,password)
-        await Basepage.findValue(expectedValue)
-        await Basepage.checkValue(expectedValue, realvalue)
+    it('search hk50 test', async function() {
+        await driver.get("https://my.liteforex.com/ru?openPopup=%2Fru%2Flogin%2Fpopup%3FreturnUrl%3D%252Fru%252F")
+        await driver.manage().window().setRect(1084, 697)
+        await driver.sleep(3000);
+        await driver.findElement(By.id("loginform-login")).sendKeys(login)
+        await driver.findElement(By.id("loginform-password")).sendKeys(password)
+        await driver.findElement(By.css(".btn_large:nth-child(1)")).click()
+        await driver.sleep(3000);
+        await driver.findElement(By.name("search")).click()
+        await driver.findElement(By.name("search")).sendKeys(expectedValue)
+        await driver.sleep(3000);
+
+        await driver.findElement(By.css(".item:nth-child(2) > .link")).click()
+        await driver.sleep(3000);
+
+        realvalue = await driver.findElement(By.css(".page_header_part > .title > h2")).getText()
+        assert.equal(expectedValue, realvalue);
+
     })
 })
